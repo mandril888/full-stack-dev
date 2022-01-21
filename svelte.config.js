71c6d5bb9c1efeffb8736d955a8bc27170450224
Arturo Rubio Infante <arturo.rubio.89.8@gmail.com>
2022-01-21 19:16:04 +0000

    Add { to vite > hmr config

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,7 @@ const config = {
 		target: '#svelte',
 		vite: {
 			server: {
-				hmr {
+				hmr: {
 					clientPort: process.env.HMR_HOST ? 443 : 3000,
 					host: process.env.HMR_HOST ? process.env.HMR_HOST.substring("https://".length) = "localhost"
 				}
